71e0cd0048b898788b8b36376e9c425be34111e4
Drake Sanders <drake_sanders12@icloud.com>
2021-12-05 23:27:58 -0600

    first commit

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect, session
+app = Flask(__name__)
+app.secret_key = 'yomama'
+@app.route('/')
+def index():
+    if 'count' not in session:
+        session['count'] = 1
+    else:
+        session['count'] += 1
+    return render_template('index.html')
+
+@app.route('/count')
+def count():
+    session['count'] += 1
+    return redirect('/')
+
+@app.route('/destroy_session')
+def destroy():
+    session.clear()
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
